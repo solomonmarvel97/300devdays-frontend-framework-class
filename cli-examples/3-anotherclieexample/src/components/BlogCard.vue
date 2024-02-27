895dc3887488a2c782123a8605ccf0a6ab56a220
Marvelous Solomon <solomonmarvel@hotmail.com>
2024-02-27 23:02:10 +0100

    added cli examples

---
+++ cli-examples/3-anotherclieexample/src/components/BlogCard.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="card">
+        <img :src="imageSrc"
+            :alt="altText">
+        <h2>{{title}}</h2>
+        <p>{{description}}</p>
+        <button class="btn" @click="buttonClicked"> Read Blog </button>
+    </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      imageSrc: {
+        type: String,
+        required: true
+      },
+      altText: {
+        type: String,
+        required: true
+      },
+      title: {
+        type: String,
+        required: true
+      },
+      description: {
+        type: String,
+        required: true
+      },
+    },
+    methods: {
+      buttonClicked() {
+        alert('you clicked on read blog')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.card {
+    max-width: 300px;
+    height: 100%;
+    position: relative;
+    padding: 12px;
+    border-radius: 12px
+}
+
+.card img {
+    height: 200px;
+    width: 100%;
+    object-fit: cover;
+    margin-bottom: 1em;
+    border-radius: 6px
+}
+
+.card h2 {
+    color: #1e1e1e;
+    font-size: 16px;
+    margin-bottom: 1em
+}
+
+.card p {
+    color: #1e1e1e;
+    font-size: 14px;
+    margin-bottom: 10px
+}
+
+.btn {
+    width: 100%;
+    padding: 16px;
+    border: none;
+    background: #1e1e1e;
+    color: white;
+    border-radius: 4px;
+    cursor: pointer
+}
+</style>
---
+++ cli-examples/3-anotherclieexample/src/views/HomeView.vue
@@ -0,0 +1,71 @@
+<script setup>
+import BlogCard from '../components/BlogCard.vue'
+</script>
+
+<template>
+    <main>
+        <div class="blogs">
+            <BlogCard v-for="blog in blogs" :imageSrc="blog.imageSrc" :altText="blog.altText" :title="blog.title"
+                :description="blog.description" />
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            blogs: [
+                {
+                    id: 1,
+                    imageSrc: "https://images.unsplash.com/photo-1671726203422-dd710fd8a72d?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwcm9maWxlLXBhZ2V8Mnx8fGVufDB8fHx8fA%3D%3D",
+                    altText: "Blog Item 1",
+                    title: "Blog Item 1",
+                    description: "This is an intro blog teaching us html"
+                },
+
+                {
+                    id: 2,
+                    imageSrc: "https://images.unsplash.com/photo-1671726203449-34e89df45211?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwcm9maWxlLXBhZ2V8MTB8fHxlbnwwfHx8fHw%3D",
+                    altText: "Blog Item 2",
+                    title: "Blog Item 2",
+                    description: "This is an intro blog teaching us css"
+                },
+
+                {
+                    id: 3,
+                    imageSrc: "https://images.unsplash.com/photo-1671726203379-692599a6e70d?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwcm9maWxlLXBhZ2V8OXx8fGVufDB8fHx8fA%3D%3D",
+                    altText: "Blog Item 3",
+                    title: "Blog Item 3",
+                    description: "This is an intro blog teaching us nodejs"
+                },
+
+                {
+                    id: 4,
+                    imageSrc: "https://images.unsplash.com/photo-1671726203486-9b7ac1893ba1?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwcm9maWxlLXBhZ2V8MTF8fHxlbnwwfHx8fHw%3D",
+                    altText: "Blog Item 4",
+                    title: "Blog Item 4",
+                    description: "This is an intro blog teaching us vuejs"
+                },
+
+                {
+                id: 5,
+                imageSrc: "https://images.unsplash.com/photo-1671726203486-9b7ac1893ba1?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwcm9maWxlLXBhZ2V8MTF8fHxlbnwwfHx8fHw%3D",
+                altText: "Blog Item 5",
+                title: "Blog Item 5",
+                description: "This is an intro blog teaching us react"
+                },
+            ]
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+.blogs {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+    gap: 30px
+}
+</style>
